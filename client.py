f091715cf5362ec02a29a602a7a435beb3163131
Amit Kumar Singh <snghamit12@gmail.com>
2020-09-16 00:45:55 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("127.0.0.1", 12345))
+msg = ""
+while msg != "exit":
+   msg = s.recv(1024)
+   print("server: ", msg.decode("utf-8"))
+   my = input("client: ")
+   s.send(bytes(my, "utf-8"))
